24293c8cbd53ecf139869e5479013819d341855e
Lokesh-1-R <83818096+Lokesh-1-R@users.noreply.github.com>
2023-01-24 13:44:36 +0530

    😎

---
+++ Java/Abstraction/src/Bank.java
@@ -0,0 +1,7 @@
+
+abstract class Bank{	
+	public abstract void Account(String Number,double Balance,String Name,String Email,String PhoneNumber);	
+	public abstract void DepositMoney(double DepositedMoney);
+	public abstract void WithDrawMoney(double WithdrawalMoney);
+	
+}
